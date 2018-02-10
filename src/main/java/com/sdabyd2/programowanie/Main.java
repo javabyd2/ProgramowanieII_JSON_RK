1c6fcb3203cb0b21865d9a1d129655228244fcc0
Radek Kryszewski <radoslaw246@wp.pl>
2018-02-10 12:46:36 +0100

    json

--- src/main/java/com/sdabyd2/programowanie/Main.java
+++ src/main/java/com/sdabyd2/programowanie/Main.java
@@ -1,8 +1,38 @@
 package com.sdabyd2.programowanie;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sdabyd2.programowanie.Model.Item;
+import com.sdabyd2.programowanie.Model.Order;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException{
+
+
+        Item item = new Item("mleko","3","TESCO");
+        Item item1 = new Item("baton","4","TESCO");
+        Item item2 = new Item("gruszki","5","TESCO");
+
+
+        List<Item> itemList = Arrays.asList(item, item1, item2);
+
+        Order order = new Order(itemList, 1);
+
+        ObjectMapper mapper = new ObjectMapper();
+        File fileName = new File ("order.json");
+        fileName.createNewFile();
+        mapper.writeValue(fileName, order);
+
+        Order readOrders = mapper.readValue(new File("order.json"), Order.class);
+
+
+        for(Item items: readOrders.getItemList()){
+            System.out.println(items.getName());
+        }
     }
 }
---
+++ src/main/java/com/sdabyd2/programowanie/Model/Item.java
@@ -0,0 +1,43 @@
+package com.sdabyd2.programowanie.Model;
+
+public class Item {
+
+    private String name;
+    private String price;
+    private String shop;
+
+    public Item(){}
+
+    public Item(String name, String price, String shop) {
+        this.name = name;
+        this.price = price;
+        this.shop = shop;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getShop() {
+        return shop;
+    }
+
+    public void setShop(String shop) {
+        this.shop = shop;
+    }
+
+
+}
